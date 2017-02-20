9c5ce6f23fe96de8dbdc7ac25a9e81db9b44841a
EthanArmbrust <ethanarm@buffalo.edu>
2017-02-20 14:00:43 -0500

    Fixed the mandlebrot set to include correct range for values.

--- src/pixelMatrix/PixelMatrix.java
+++ src/pixelMatrix/PixelMatrix.java
@@ -9,12 +9,16 @@ public class PixelMatrix {
 	 */
 	
 	int[][] _m;
+	int _xDim;
+	int _yDim;
 	
 	public PixelMatrix(int x, int y){
 		if (x<0) {x = x * -1;}
 		if (y<0) {y = y * -1;}
 		int[][] matrix = new int[x][y];
 		_m = matrix;
+		_xDim = x;
+		_yDim = y;
 	}
 	
 	public int[][] giveGrid() {
@@ -22,23 +26,28 @@ public class PixelMatrix {
 		retVal = _m;
 		return retVal;
 	}
-	
-	public int[][] mandlebrotEscapes(int distance, int maxSteps){
+	// make sure every set has these parameters 
+	public int[][] mandlebrotEscapes(double distance, double maxSteps, double xMin, double xMax, double yMin, double yMax ){
 		int[][] retVal = new int[_m.length][_m[0].length];
+		
+			
+		//This needs to be in every set 
+		double xStep = (xMax - xMin) / _xDim;
+		double yStep = (yMax - yMin) / _yDim;
 		
 		for(int x = 0; x < _m.length; x++){
 			for(int y = 0; y < _m.length; y++){
-				int xCalc = x;
-				int yCalc = y;
+				double xCalc = xMin + (x * xStep);
+				double yCalc = yMin + (y * yStep);
 				
 				double dist = Math.sqrt((x*x) + (y*y));
 				int passes = 0;
 				while(dist <= distance && passes < maxSteps){
-					int tempX = xCalc;
-					int tempY = yCalc;
+					double tempX = xCalc;
+					double tempY = yCalc;
 					//change these for each different set
 					xCalc = (tempX * tempX) - (tempY * tempY) + tempX;
-					yCalc = 2 * xCalc * yCalc + tempY;
+					yCalc = 2 * tempX * tempY + tempY;
 					
 					passes++;
 					dist = Math.sqrt((xCalc*xCalc) + (yCalc*yCalc));	
@@ -66,7 +75,7 @@ public class PixelMatrix {
 					double tempY = yCalc;
 					//change these for each different set
 					xCalc = (tempX * tempX) - (tempY * tempY) - 0.72689;
-					yCalc = 2 * xCalc * yCalc + 0.188887;
+					yCalc = 2 * tempX * tempY + 0.188887;
 					
 					passes++;
 					dist = Math.sqrt((xCalc*xCalc) + (yCalc*yCalc));	
@@ -79,8 +88,9 @@ public class PixelMatrix {
 	
 	}
 	
-	 public int[][] burningShipEscapes(int distance, int maxSteps){
+	 public int[][] burningShipEscapes(int distance, int maxSteps, double xMin, double xMax, double yMin, double yMax){
 			int[][] retVal = new int[_m.length][_m[0].length];
+			
 			
 			for(int x = 0; x < _m.length; x++){
 				for(int y = 0; y < _m.length; y++){
