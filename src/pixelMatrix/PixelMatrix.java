d958738713ac8d469ea85caa45b54fc478ebb1dd
Brett Baker <brettmatt8@hotmail.com>
2017-02-21 11:22:58 -0500

    Fixed the burningShipEscape
    changed the escape to match the range of x and y values with those
    specified

--- src/pixelMatrix/PixelMatrix.java
+++ src/pixelMatrix/PixelMatrix.java
@@ -2,11 +2,6 @@ package pixelMatrix;
 
 public class PixelMatrix {
 	
-	/*
-	 * note from Brett: I added the if statements in the class definition because I think that
-	 * the definition failed  my negative number test and I added the method giveGrid so that I 
-	 * could access the _m variable from my test class
-	 */
 	
 	int[][] _m;
 	int _xDim;
@@ -88,30 +83,32 @@ public class PixelMatrix {
 	
 	}
 	
-	 public int[][] burningShipEscapes(int distance, int maxSteps, double xMin, double xMax, double yMin, double yMax){
-			int[][] retVal = new int[_m.length][_m[0].length];
+	public int[][] burningShipEscapes(double distance, double maxSteps, double xMin, double xMax, double yMin, double yMax){
+		int[][] retVal = new int[_m.length][_m[0].length];
 			
+		double xStep = (xMax - xMin) / _xDim;
+		double yStep = (yMax - yMin) / _yDim;
 			
-			for(int x = 0; x < _m.length; x++){
-				for(int y = 0; y < _m.length; y++){
-					int xCalc = x;
-					int yCalc = y;
-					
-					double dist = Math.sqrt((x*x) + (y*y));
-					int passes = 0;
-					while(dist <= distance && passes < maxSteps){
-						int tempX = xCalc;
-						int tempY = yCalc;
-						xCalc = (tempX * tempX) - (tempY * tempY) + tempX;
-						yCalc = Math.abs(2*tempX*tempY);
-						passes++;
-						dist = Math.sqrt((xCalc*xCalc) + (yCalc*yCalc));	
-					}
-					retVal[x][y] = passes;
+		for(int x = 0; x < _m.length; x++){
+			for(int y = 0; y < _m.length; y++){
+				double xCalc = xMin + (x * xStep);
+				double yCalc = yMin + (y * yStep);
+				
+				double dist = Math.sqrt((x*x) + (y*y));
+				int passes = 0;
+				while(dist <= distance && passes < maxSteps){
+					double tempX = xCalc;
+					double tempY = yCalc;
+					xCalc = (tempX * tempX) - (tempY * tempY) + tempX;
+					yCalc = Math.abs(2*tempX*tempY);
+					passes++;
+					dist = Math.sqrt((xCalc*xCalc) + (yCalc*yCalc));	
 				}
+				retVal[x][y] = passes;
 			}
-			return retVal;
 		}
+		return retVal;
+	}
 	
 	
 }
