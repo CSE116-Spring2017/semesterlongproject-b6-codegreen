67cc4270a9318c2ecea576d45161ef4d3a5ab621
Brett Baker <brettmatt8@hotmail.com>
2017-02-24 14:18:34 -0500

    Added pixel translation test and grid size check

--- src/tests/MandelbrotEscapesTest.java
+++ src/tests/MandelbrotEscapesTest.java
@@ -57,4 +57,57 @@ public class MandelbrotEscapesTest {
 		}
 		assertEquals(testGrid[xy[0]][xy[1]],1);
 	}
+	
+	/*
+	 * a test that checks the height of the matrix is correct
+	 */
+	@Test
+	public void correctLength() {
+		_canvas = new PixelMatrix(512,512);
+		int[][] mandelbrotPaint = _canvas.mandelbrotEscapes(2.0,255,-2.15,.6,-1.3,1.3);
+		assertEquals(mandelbrotPaint.length,512);
+	}
+	
+	/*
+	 * a test that checks the width of the matrix is correct
+	 */
+	@Test
+	public void correctWidth() {
+		_canvas = new PixelMatrix(512,512);
+		int[][] mandelbrotPaint = _canvas.mandelbrotEscapes(2.0,255,-2.15,.6,-1.3,1.3);
+		assertEquals(mandelbrotPaint[0].length,512);
+	}
+	
+	/*
+	 * a test that checks if the value of a random chosen x and y coordinate matches the calculated value
+	 */
+	@Test
+	public void pixelCoordinateTranslation() {
+		double dist = 0.0;
+		int passes = 0;
+		_canvas = new PixelMatrix(512,512);
+		int[][] constant = _canvas.mandelbrotEscapes(2,255,-2.15,.6,-1.3,1.3);
+		
+		double xStep = (.6 - -2.15) / 512;
+		double yStep = (1.3 - -1.3) / 512;
+		
+		double xCalc = -2.15 + (45 * xStep);
+		double yCalc = 1.3 - (23 * yStep);
+		
+		dist = Math.sqrt((xCalc*xCalc) + (yCalc*yCalc));
+		
+		while(dist <= 2 && passes < 255) {
+			double tempX = xCalc;
+			double tempY = yCalc;
+			
+			xCalc = (tempX * tempX) - (tempY * tempY) + tempX;
+			yCalc = 2 * tempX * tempY + tempY;
+			
+			passes++;
+			dist = Math.sqrt((xCalc*xCalc) + (yCalc*yCalc));	
+		}
+		System.out.println(passes+" "+constant[45][23]);
+		
+		assertEquals(constant[45][23],passes);
+	}
 }
