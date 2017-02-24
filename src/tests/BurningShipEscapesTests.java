2fe24313236195728a04bec88f9a480957990111
Mohammed Sefath Chowdhury <Mchowdhury57@gmail.com>
2017-02-24 17:36:16 -0500

    pixelCoordinateTranslationTests for every fractal set

--- src/tests/BurningShipEscapesTests.java
+++ src/tests/BurningShipEscapesTests.java
@@ -10,6 +10,109 @@ public class BurningShipEscapesTests {
 	
 	PixelMatrix _canvas;
 	
+	/*
+	 * A series of tests that checks if the value of a random chosen x and y coordinate
+	 * matches the calculated value
+	 */
+	@Test
+	public void pixelCoordinateTranslation() {
+		double dist = 0.0;
+		int passes = 0;
+		_canvas = new PixelMatrix(512, 512);
+		int[][] constant = _canvas.juliaEscapes(2, 255, -1.8, 1.7, -0.08, 0.025);
+
+		double xStep = (1.7 - (-1.8)) / 512;
+		double yStep = (0.025 - (-0.08)) / 512;
+
+		double xCalc = -1.8 + (0 * xStep); 
+		double yCalc = 0.025 - (0 * yStep); 
+
+		dist = Math.sqrt((xCalc * xCalc) + (yCalc * yCalc));
+
+		while (dist <= 2 && passes < 255) {
+			double tempX = xCalc;
+			double tempY = yCalc;
+			double origX = xCalc;
+			double origY = yCalc;
+
+			xCalc = (tempX * tempX) - (tempY * tempY) + origX;
+			yCalc = Math.abs(2*tempX*tempY) + origY;
+
+			passes++;
+			dist = Math.sqrt((xCalc * xCalc) + (yCalc * yCalc));
+		}
+		System.out.println(passes + " " + constant[0][0]);
+
+		// point (0,0)
+		assertEquals(constant[0][0], passes);
+	}
+	
+	@Test
+	public void pixelCoordinateTranslation2() {
+		double dist = 0.0;
+		int passes = 0;
+		_canvas = new PixelMatrix(512, 512);
+		int[][] constant = _canvas.juliaEscapes(2, 255, -1.8, 1.7, -0.08, 0.025);
+
+		double xStep = (1.7 - (-1.8)) / 512;
+		double yStep = (0.025 - (-0.08)) / 512;
+
+		double xCalc = -1.8 + (45 * xStep); 
+		double yCalc = 0.025 - (23 * yStep); 
+
+		dist = Math.sqrt((xCalc * xCalc) + (yCalc * yCalc));
+
+		while (dist <= 2 && passes < 255) {
+			double tempX = xCalc;
+			double tempY = yCalc;
+			double origX = xCalc;
+			double origY = yCalc;
+
+			xCalc = (tempX * tempX) - (tempY * tempY) + origX;
+			yCalc = Math.abs(2*tempX*tempY) + origY;
+
+			passes++;
+			dist = Math.sqrt((xCalc * xCalc) + (yCalc * yCalc));
+		}
+		System.out.println(passes + " " + constant[45][23]);
+
+		// random point (x, y)
+		assertEquals(constant[45][23], passes);
+	}
+	
+	@Test
+	public void pixelCoordinateTranslation3() {
+		double dist = 0.0;
+		int passes = 0;
+		_canvas = new PixelMatrix(512, 512);
+		int[][] constant = _canvas.juliaEscapes(2, 255, -1.8, 1.7, -0.08, 0.025);
+
+		double xStep = (1.7 - (-1.8)) / 512;
+		double yStep = (0.025 - (-0.08)) / 512;
+
+		double xCalc = -1.8 + (512 * xStep); 
+		double yCalc = 0.025 - (512 * yStep);
+		
+		dist = Math.sqrt((xCalc * xCalc) + (yCalc * yCalc));
+
+		while (dist <= 2 && passes < 255) {
+			double tempX = xCalc;
+			double tempY = yCalc;
+			double origX = xCalc;
+			double origY = yCalc;
+
+			xCalc = (tempX * tempX) - (tempY * tempY) + origX;
+			yCalc = Math.abs(2*tempX*tempY) + origY;
+
+			passes++;
+			dist = Math.sqrt((xCalc * xCalc) + (yCalc * yCalc));
+		}
+		System.out.println(passes + " " + constant[511][511]);
+
+		// point (512, 512)
+		assertEquals(constant[511][511], passes);
+	}
+	
 	@Test
 	public void noZeroOrOne() {
 		PixelMatrix p = new PixelMatrix(512,512);
--- src/tests/MandelbrotEscapesTest.java
+++ src/tests/MandelbrotEscapesTest.java
@@ -13,6 +13,112 @@ import code.PixelMatrix;
 
 public class MandelbrotEscapesTest {
 	
+
+	/*
+	 * A series of tests that checks if the value of a random chosen x and y coordinate
+	 * matches the calculated value
+	 */
+	
+	@Test
+	public void pixelCoordinateTranslationTest1() {
+		double dist = 0.0;
+		int passes = 0;
+		_canvas = new PixelMatrix(512, 512);
+		int[][] constant = _canvas.mandelbrotEscapes(2,255,-2.15,.6,-1.3,1.3);
+
+		double xStep = (.6 - -2.15) / 512;
+		double yStep = (1.3 - -1.3) / 512;
+		
+		double xCalc = -2.15 + (45 * xStep);
+		double yCalc = 1.3 - (23 * yStep);
+
+		dist = Math.sqrt((xCalc * xCalc) + (yCalc * yCalc));
+
+		while (dist <= 2 && passes < 255) {
+			double tempX = xCalc;
+			double tempY = yCalc;
+			double origX = xCalc;
+			double origY = yCalc;
+
+			xCalc = (tempX * tempX) - (tempY * tempY) + origX;
+			yCalc = 2 * tempX * tempY + origY;
+
+			passes++;
+			dist = Math.sqrt((xCalc * xCalc) + (yCalc * yCalc));
+		}
+		System.out.println(passes + " " + constant[0][0]);
+
+		// point (0,0)
+		assertEquals(constant[0][0], passes);
+	}
+	
+	@Test
+	public void pixelCoordinateTranslationTest2() {
+		double dist = 0.0;
+		int passes = 0;
+		_canvas = new PixelMatrix(512, 512);
+		int[][] constant = _canvas.mandelbrotEscapes(2,255,-2.15,.6,-1.3,1.3);
+
+		double xStep = (.6 - -2.15) / 512;
+		double yStep = (1.3 - -1.3) / 512;
+		
+		double xCalc = -2.15 + (89 * xStep);
+		double yCalc = 1.3 - (26 * yStep);
+
+		dist = Math.sqrt((xCalc * xCalc) + (yCalc * yCalc));
+
+		while (dist <= 2 && passes < 255) {
+			double tempX = xCalc;
+			double tempY = yCalc;
+			double origX = xCalc;
+			double origY = yCalc;
+
+			xCalc = (tempX * tempX) - (tempY * tempY) + origX;
+			yCalc = 2 * tempX * tempY + origY;
+
+			passes++;
+			dist = Math.sqrt((xCalc * xCalc) + (yCalc * yCalc));
+		}
+		System.out.println(passes + " " + constant[89][26]);
+
+		// point (89,26)
+		assertEquals(constant[89][26], passes);
+	}
+	
+	@Test
+	public void pixelCoordinateTranslationTest3() {
+		double dist = 0.0;
+		int passes = 0;
+		_canvas = new PixelMatrix(512, 512);
+		int[][] constant = _canvas.mandelbrotEscapes(2,255,-2.15,.6,-1.3,1.3);
+
+		double xStep = (.6 - -2.15) / 512;
+		double yStep = (1.3 - -1.3) / 512;
+		
+		double xCalc = -2.15 + (512 * xStep);
+		double yCalc = 1.3 - (512 * yStep);
+
+		dist = Math.sqrt((xCalc * xCalc) + (yCalc * yCalc));
+
+		while (dist <= 2 && passes < 255) {
+			double tempX = xCalc;
+			double tempY = yCalc;
+			double origX = xCalc;
+			double origY = yCalc;
+
+			xCalc = (tempX * tempX) - (tempY * tempY) + origX;
+			yCalc = 2 * tempX * tempY + origY;
+
+			passes++;
+			dist = Math.sqrt((xCalc * xCalc) + (yCalc * yCalc));
+		}
+		System.out.println(passes + " " + constant[511][511]);
+
+		// point (512,512)
+		assertEquals(constant[511][511], passes);
+	}
+		
+	
 	/*
 	 * create an instance of the matrix painting class
 	 */
