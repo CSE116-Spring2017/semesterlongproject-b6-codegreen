2cfcca4a55d7244acc218c197febe1f33028eade
bmbaker <bmbaker@mace.cse.buffalo.edu>
2017-03-16 19:35:33 -0400

    Trying to fix

--- src/observer_pattern/Observable.java
+++
@@ -1,8 +0,0 @@
-package observer_pattern;
-
-public interface Observable {
-	
-	public void notifyObservers();
-	public void addObserver(Observer o);
-
-}
--- src/observer_pattern/Observer.java
+++
@@ -1,7 +0,0 @@
-package observer_pattern;
-
-public interface Observer {
-
-	public void update();
-	
-}
